cd1bfb7e22efb80db244894a05a228a5f2c20baf
yilbegan <w2v@protonmail.com>
2021-11-14 13:22:12 +0300

    :bug: Fix queries

--- app/processing/__init__.py
+++ app/processing/__init__.py
@@ -2,7 +2,7 @@ from .pipeline import get_queries
 
 
 def process_query(query: str) -> list[str]:
-    return get_queries(query, 5)
+    return get_queries(query, 3)
 
 
 def normalize_tag(tag: str) -> list[str]:
